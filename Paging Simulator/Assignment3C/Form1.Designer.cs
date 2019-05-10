7a6ef49280a6ac65d1f0f4480c5a72e19be19dd2
carlosilveraxe <50480011+carlosilveraxe@users.noreply.github.com>
2019-05-10 10:34:18 +0200

    Add files via upload

---
+++ Paging Simulator/Assignment3C/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Assignment3C
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Process = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kept_Page = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Page_Out = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Page_In = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Memory_Block = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Memory_Block.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Index,
+            this.Process,
+            this.Kept_Page,
+            this.Page_Out,
+            this.Page_In});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 103);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(376, 301);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Index
+            // 
+            this.Index.HeaderText = "Index";
+            this.Index.Name = "Index";
+            this.Index.ReadOnly = true;
+            this.Index.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Index.Width = 40;
+            // 
+            // Process
+            // 
+            this.Process.HeaderText = "Process";
+            this.Process.Name = "Process";
+            this.Process.ReadOnly = true;
+            this.Process.Width = 80;
+            // 
+            // Kept_Page
+            // 
+            this.Kept_Page.HeaderText = "Kept Page";
+            this.Kept_Page.Name = "Kept_Page";
+            this.Kept_Page.ReadOnly = true;
+            this.Kept_Page.Width = 90;
+            // 
+            // Page_Out
+            // 
+            this.Page_Out.HeaderText = "Page Out";
+            this.Page_Out.Name = "Page_Out";
+            this.Page_Out.ReadOnly = true;
+            this.Page_Out.Width = 80;
+            // 
+            // Page_In
+            // 
+            this.Page_In.HeaderText = "Page In";
+            this.Page_In.Name = "Page_In";
+            this.Page_In.ReadOnly = true;
+            this.Page_In.Width = 80;
+            // 
+            // Memory_Block
+            // 
+            this.Memory_Block.Controls.Add(this.button4);
+            this.Memory_Block.Controls.Add(this.button3);
+            this.Memory_Block.Controls.Add(this.button2);
+            this.Memory_Block.Controls.Add(this.button1);
+            this.Memory_Block.Location = new System.Drawing.Point(468, 232);
+            this.Memory_Block.Name = "Memory_Block";
+            this.Memory_Block.Size = new System.Drawing.Size(200, 164);
+            this.Memory_Block.TabIndex = 1;
+            this.Memory_Block.TabStop = false;
+            this.Memory_Block.Text = "Memory Block";
+            this.Memory_Block.Enter += new System.EventHandler(this.Memory_Block_Enter);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 127);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(188, 30);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 30);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 30);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 30);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(217, 429);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(156, 41);
+            this.button9.TabIndex = 2;
+            this.button9.Text = "Manual Addition";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(379, 429);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(156, 41);
+            this.button10.TabIndex = 3;
+            this.button10.Text = "Automatic Simulation";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(541, 429);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(156, 41);
+            this.button11.TabIndex = 4;
+            this.button11.Text = "Clear Table";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 438);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Paging Algorithm: FIFO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(487, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Total Processes: 160";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(487, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Total Pages: 16";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(487, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Processes Per Page: 10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(487, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Total Memory Blocks: 4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 519);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.Memory_Block);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Paging Simulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Memory_Block.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox Memory_Block;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Process;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kept_Page;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Page_Out;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Page_In;
+    }
+}
+
---
+++ Paging Simulator/Assignment3C/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Assignment3C
+{
+    public partial class Form1 : Form
+    {
+        public int[] memoryBlocks = new int[4];
+        int FIFO = 0;
+        int indexNum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Memory_Block_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 160;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int keptPage = 0;
+            int processNum = 0;
+            string pageOut;
+            Random rand = new Random();
+            Button[] array = { button1, button2, button3, button4 };
+
+            for (int i=0; i<160; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = i;
+
+                processNum = rand.Next(1, 161);
+                
+                dataGridView1.Rows[i].Cells[1].Value = processNum;
+
+                if (processNum < 11)
+                    keptPage = 0;
+                else
+                    keptPage = processNum / 10;
+
+                dataGridView1.Rows[i].Cells[2].Value = keptPage;
+
+                if(keptPage!= memoryBlocks[0] && keptPage != memoryBlocks[1] && keptPage != memoryBlocks[2] 
+                    && keptPage != memoryBlocks[3])
+                {
+                    memoryBlocks[FIFO] = keptPage;
+
+                    pageOut = array[FIFO].Text;
+                    dataGridView1.Rows[i].Cells[3].Value = pageOut;
+
+                    array[FIFO].Text = Convert.ToString(keptPage);
+                    FIFO++;
+
+                    if(FIFO==4)
+                    {
+                        FIFO = 0;
+                    }
+                }
+
+                dataGridView1.Rows[i].Cells[4].Value = keptPage;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button[] array = { button1, button2, button3, button4 };
+
+            for (int i=0; i<160; i++)
+            {
+                for(int y=0; y<5; y++)
+                {
+                    dataGridView1.Rows[i].Cells[y].Value = "";
+                }
+            }
+
+            for(int i=0; i<4; i++)
+            {
+                array[i].Text = "";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int keptPage = 0;
+            int processNum = 0;
+            //int i = 0;
+            string pageOut;
+            Random rand = new Random();
+            Button[] array = { button1, button2, button3, button4 };
+
+            dataGridView1.Rows[indexNum].Cells[0].Value = indexNum;
+
+            processNum = rand.Next(0, 161);
+            dataGridView1.Rows[indexNum].Cells[1].Value = processNum;
+
+            if (processNum < 11)
+                keptPage = 0;
+            else
+                keptPage = processNum / 10;
+
+            dataGridView1.Rows[indexNum].Cells[2].Value = keptPage;
+
+            if (keptPage != memoryBlocks[0] && keptPage != memoryBlocks[1] && keptPage != memoryBlocks[2] && keptPage != memoryBlocks[3])
+            {
+                memoryBlocks[FIFO] = keptPage;
+
+                pageOut = array[FIFO].Text;
+
+                if (pageOut == "")
+                {
+                    pageOut = "N/A";
+                    dataGridView1.Rows[indexNum].Cells[3].Value = pageOut;
+                }
+                else
+                {
+                    dataGridView1.Rows[indexNum].Cells[3].Value = pageOut;
+                }
+
+                array[FIFO].Text = Convert.ToString(keptPage);
+                FIFO++;
+
+                if (FIFO == 4)
+                {
+                    FIFO = 0;
+                }
+            }
+
+            dataGridView1.Rows[indexNum].Cells[4].Value = keptPage;
+
+            indexNum++;
+        }
+
+        public static void mainThread()
+        {
+            Thread.Sleep(10);
+        }
+    }
+}
